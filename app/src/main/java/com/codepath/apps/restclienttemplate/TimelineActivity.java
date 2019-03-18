009a298b2ec28eaf822af1a2ac7d42734a0fa142
paul_alarcon <pa.f.e@hotmail.com>
2019-03-18 19:33:30 -0400

    Second

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,11 +1,15 @@
 package com.codepath.apps.restclienttemplate;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
@@ -20,6 +24,8 @@ import java.util.List;
 import cz.msebera.android.httpclient.Header;
 
 public class TimelineActivity extends AppCompatActivity {
+
+    private final int REQUEST_CODE = 20;
 
     private TwitterClient client;
     private RecyclerView rvTweets;
@@ -60,6 +66,32 @@ public class TimelineActivity extends AppCompatActivity {
                 populateHomeTimeLine();
             }
         });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.compose){
+//            Toast.makeText(this, "Compose", Toast.LENGTH_SHORT).show();
+            Intent ComposeActivity = new Intent(this, ComposeActivity.class);
+            startActivityForResult(ComposeActivity, REQUEST_CODE);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+           Tweet tweet = data.getParcelableExtra("tweet");
+           tweets.add(0, tweet);
+           adapter.notifyItemInserted(0);
+           rvTweets.smoothScrollToPosition(0);
+        }
     }
 
     private void populateHomeTimeLine() {
